5aa5a84fb48e16fe7fb1bd1aaa1515c1640c7887
Luisa Rojas <r.luisaf@gmail.com>
2020-12-01 16:51:54 -0500

    Added more tests and improved solution

--- challenges/apple-stocks/solution.py
+++ challenges/apple-stocks/solution.py
@@ -1,20 +1,15 @@
 # Solution
 def get_max_profit(stock_prices):
-    
-    if len(stock_prices) < 2:
-        raise Exception('Not enough data in stock prices list')
-    
-    max_price = max(stock_prices)
-    # Pick the max value furthest to the left if there is more than one
-    max_price_index = max([i for i, price in enumerate(stock_prices) if price == max_price])
-    
-    # If the max price is the first in the list
-    if max_price_index == 0:
-        return max(stock_prices[max_price_index+1:]) - max_price
-    
-    # Look for the cheapest the stock was previous to the time it was at its max
-    min_price = max_price
-    for price in stock_prices[:max_price_index]:
-        if price < min_price:
-            min_price = price
-    return max_price - min_price
+
+	if len(stock_prices) < 2: return 0
+
+	max_profit = 0 # Shouldn't make less than 0
+	min_price = stock_prices[0]
+
+	for price in stock_prices:
+		if price < min_price:
+			min_price = price
+		if (price - min_price) > max_profit:
+			max_profit = (price - min_price)
+			
+	return max_profit
--- challenges/apple-stocks/test_solution.py
+++ challenges/apple-stocks/test_solution.py
@@ -3,13 +3,34 @@ from solution import get_max_profit
 
 # Tests
 def test_single_stock():
-    stock_prices = [9]
-    with pytest.raises(Exception): get_max_profit(stock_prices)
+	stock_prices = [9]
+	assert get_max_profit(stock_prices) == 0
+	# with pytest.raises(SingleStockPrice): get_max_profit(stock_prices)
 
-def test_negative_profit():
-    stock_prices = [11, 7, 6, 6, 10, 9]
-    assert get_max_profit(stock_prices) == -1
+# def test_negative_profit():
+#     stock_prices = [11, 7, 6, 6, 10, 9]
+#     assert get_max_profit(stock_prices) == -1
 
 def test_positive_profit():
-    stock_prices = [10, 7, 5, 8, 11, 9]
-    assert get_max_profit(stock_prices) == 6
+	stock_prices = [10, 7, 5, 8, 11, 9]
+	assert get_max_profit(stock_prices) == 6
+
+def test_no_gain():
+	stock_prices = [7,6,4,3,1]
+	assert get_max_profit(stock_prices) == 0
+
+def test_empty_sequence():
+	stock_prices = []
+	assert get_max_profit(stock_prices) == 0
+
+def test_multiple_max_prices():
+	stock_prices = [2,1,2,1,0,1,2]
+	assert get_max_profit(stock_prices) == 2
+
+def test_max_before_min_1():
+	stock_prices = [3,3,5,0,0,3,1,4]
+	assert get_max_profit(stock_prices) == 4
+
+def test_max_before_min_2():
+	stock_prices = [3,2,6,5,0,3]
+	assert get_max_profit(stock_prices) == 4
